be8d8c86499ef9303d051e140e74e524839a51f8
Jay Wang <jaywang007@yahoo.com>
2018-05-25 12:55:26 -0500

    initialize and setting up error handler

---
+++ warbler-server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT =  8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all my routes
+app.use(function(req, res, next){
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, function(){
+  console.log(`Server is starting on port ${PORT}`);
+})
