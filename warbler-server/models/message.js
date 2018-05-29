63649251028c9a9db29e0d27cdab492cec74f283
Jay Wang <jaywang007@yahoo.com>
2018-05-28 23:17:30 -0500

    user can create a message

--- warbler-server/models/message.js
+++ warbler-server/models/message.js
@@ -17,9 +17,9 @@ const messageSchema = new mongoose.Schema({
 messageSchema.pre('remove', async function(next) {
   try {
     // find a user
-    let user = await User.findById(this.userId);
+    let user = await User.findById(this.user);
     // remove the id of the message from their messages list
-    user.message.remove(this.id);
+    user.messages.remove(this.id);
     // save that user
     await user.save();
     // return next 
@@ -30,4 +30,4 @@ messageSchema.pre('remove', async function(next) {
 })
 
 const Message = mongoose.model("Message", messageSchema);
-mongoose.exports = Message;
+module.exports = Message;
