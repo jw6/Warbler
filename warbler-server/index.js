0b343296cfb1d4b24acc536607ee8c238518e92b
Jay Wang <jaywang007@yahoo.com>
2018-05-30 23:27:05 -0500

    handle unauthenticated messaging and authorized messaging

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/message");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const db = require("./models");
 const PORT = 8081;
 
@@ -13,7 +14,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users/:id/messages", messageRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,          // make sure you're login
+  ensureCorrectUser,      // make sure you're the correct user 
+  messageRoutes
+);
 
 app.use(function (req, res, next) {
   let err = new Error("Not Found");
--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -1,11 +1,11 @@
 require('dotenv').load();
-const jwt = require("jsonwebtoken");
+var jwt = require("jsonwebtoken");
 
 // make sure the user is logged - Authentication
-exports.loginRequired - function(req, res, next) {
+exports.loginRequired = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1]; // Bears token
-    jwt.verify(toekn, process.env.SECRET_KEY, function(err, decoded) {
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
       if(decoded) {
         return next();
       } else {
@@ -27,8 +27,8 @@ exports.loginRequired - function(req, res, next) {
 exports.ensureCorrectUser = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET.KET, function(err, decoeded) {
-      if(decoded && decoded.id === req.params.id) {
+    jwt.verify(token, process.env.SECRET.KEY, function(err, decoded) {
+      if(decoded && (decoded.id === req.params.id)) {
         return next();
       } else {
         return next({
