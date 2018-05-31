8ee320a1f51e346515fe58982971fff5692084fb
Jay Wang <jaywang007@yahoo.com>
2018-05-30 22:37:00 -0500

    add authrization function so user can only see his own messages

--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -19,11 +19,28 @@ exports.loginRequired - function(req, res, next) {
     return next({
       status: 401,
       message: "Please log in first"
-    })
+    });
   }
 }
 
 // make sure we get the correct user - Authorization
 exports.ensureCorrectUser = function(req, res, next) {
-
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET.KET, function(err, decoeded) {
+      if(decoded && decoded.id === req.params.id) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: "Unauthorized"
+        })
+      }
+    });
+  } catch (error) {
+    return next({
+          status: 401,
+          message: "Unauthorized"
+    })
+  }
 }
