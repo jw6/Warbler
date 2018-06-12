fd06f95843406b0a1f4a24d1f468c966be3620ca
Jay Wang <jaywang007@yahoo.com>
2018-06-11 19:47:38 -0500

    add homepage component and main page display

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -3,6 +3,8 @@ import {Provider} from 'react-redux';
 import {configureStore} from '../store';
 import {BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
+import Main from './Main';
+
 const store = configureStore();
 
 const App = () => (
@@ -10,6 +12,7 @@ const App = () => (
     <Router>
       <div className="onboarding">
         <Navbar />
+        <Main />
       </div>
     </Router>
   </Provider>
