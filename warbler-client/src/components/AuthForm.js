4877ee0ac63004effff669bb2df31085f7b5d2b0
Jay Wang <jaywang007@yahoo.com>
2018-06-11 20:15:35 -0500

    created AuthForm for signup and signin route

---
+++ warbler-client/src/components/AuthForm.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+export default class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      profileImageUrl: ""
+    };
+  }
+  render() {
+    const {email, username, password, profileImageUrl} = this.state;
+    const {heading, buttonText} = this.props;
+    return (
+      <div>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSumbit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              <label htmlFor="email">Email:</label>
+              <input 
+                name="email" 
+                className="form-control" 
+                id="email" 
+                onChange={this.handleChange} 
+                value={email}
+                type="text"/>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -3,11 +3,30 @@ import React from 'react';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
 const Main = props => {
   return(
     <div className="container">
       <Switch>
-        <Route exact path="/" render={props => <Homepage {...props}/>} />
+        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route exact path="/signin" render={props => {
+          return (
+            <AuthForm 
+            buttonText="Log in" 
+            header="Welcome Back." 
+            {...props} 
+            />
+          )
+        }} />
+        <Route exact path="/signin" render={props => {
+          return (
+            <AuthForm 
+              buttonText="Sign me up!" 
+              header="Join Warbler today." 
+              {...props}
+            />
+          )
+        }} />
       </Switch>
     </div>
   );
