3ca0fe471b95a099cba41546b5f4ab0df3058839
Jay Wang <jaywang007@yahoo.com>
2018-06-26 21:42:19 -0500

    added a little styling

--- warbler-client/src/containers/MessageList.js
+++ warbler-client/src/containers/MessageList.js
@@ -20,7 +20,16 @@ class MessageList extends Component {
       />
     ));
 
-    return messageList;
+    return (
+      <div className="row col-sm-8">
+        <div className="offset-1 col-sm-10">
+          <ul className="list-group" id="messages">
+            {messageList}
+          </ul>
+        </div>
+      </div>
+    )
+
   }
 }
 
